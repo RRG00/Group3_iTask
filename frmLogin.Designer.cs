c261d64fd167b889d90a735c198f09da2950ed94
Guilherme Sá <2241539@my.ipleiria.pt>
2025-05-05 23:40:43 +0100

    2ª Commit - Explicações

--- frmLogin.Designer.cs
+++ frmLogin.Designer.cs
@@ -38,54 +38,61 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(70, 42);
+            this.label1.Location = new System.Drawing.Point(105, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.Size = new System.Drawing.Size(87, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username:";
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(134, 39);
+            this.txtUsername.Location = new System.Drawing.Point(201, 60);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(138, 20);
+            this.txtUsername.Size = new System.Drawing.Size(205, 26);
             this.txtUsername.TabIndex = 1;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(134, 74);
+            this.txtPassword.Location = new System.Drawing.Point(201, 114);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(138, 20);
+            this.txtPassword.Size = new System.Drawing.Size(205, 26);
             this.txtPassword.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(70, 77);
+            this.label2.Location = new System.Drawing.Point(105, 118);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.Size = new System.Drawing.Size(82, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Password:";
             // 
             // btLogin
             // 
-            this.btLogin.Location = new System.Drawing.Point(197, 111);
+            this.btLogin.Location = new System.Drawing.Point(296, 171);
+            this.btLogin.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btLogin.Name = "btLogin";
-            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.Size = new System.Drawing.Size(112, 35);
             this.btLogin.TabIndex = 4;
             this.btLogin.Text = "Login";
             this.btLogin.UseVisualStyleBackColor = true;
+            this.btLogin.Click += new System.EventHandler(this.btLogin_Click);
             // 
             // frmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 174);
+            this.ClientSize = new System.Drawing.Size(564, 268);
             this.Controls.Add(this.btLogin);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "frmLogin";
             this.Text = "frmLogin";
             this.ResumeLayout(false);
--- frmLogin.cs
+++ frmLogin.cs
@@ -16,5 +16,18 @@ namespace iTasks
         {
             InitializeComponent();
         }
+
+        //Login Button
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            //abrir e fechar formularios
+            //Form secondForm = new frmKanban();
+            //Hide(); // esconder o form de login
+            //secondForm.ShowDialog(); // bloqueia a mudança de janela
+             
+            // criar as classes dos modelos ( 3 classes : programadores, gestores e utilizadores)
+            // bases de dados -> 
+
+        }
     }
 }
