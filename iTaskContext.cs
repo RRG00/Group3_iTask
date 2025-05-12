2915f5a6a4008e1b201a15231c5693816e1a85ca
Guilherme Sá <2241539@my.ipleiria.pt>
2025-05-12 22:19:30 +0100

    Updates

--- iTaskContext.cs
+++ iTaskContext.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace iTasks
 {
     internal class iTaskContext : DbContext
     {
        public DbSet<User> Users { get; set; }
+       public DbSet<Manager> Manager { get; set; }
+       public DbSet<Programmer> Programmers { get; set; }
        public DbSet<Task> Tasks { get; set; }
        public DbSet<TipeTask> TipeTasks { get; set; }   
     }
