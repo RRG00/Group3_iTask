c8dae09fe10a17e047b43fda60b42d3e763d9c16
Guilherme Sá <2241539@my.ipleiria.pt>
2025-05-12 21:42:16 +0100

    Ajustes
    Criação de pastas/classes

---
+++ Views/frmLogin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iTasks
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        //Login Button
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            //abrir e fechar formularios
+            //Form secondForm = new frmKanban();
+            //Hide(); // esconder o form de login
+            //secondForm.ShowDialog(); // bloqueia a mudança de janela
+             
+            // criar as classes dos modelos ( 3 classes : programadores, gestores e utilizadores)
+            // bases de dados -> 
+
+        }
+    }
+}
