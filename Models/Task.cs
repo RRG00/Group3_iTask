3e3c09cd10f3af549dce1a4b871f3f0095c82853
Guilherme Sá <2241539@my.ipleiria.pt>
2025-05-12 23:48:34 +0100

    Updates Classes

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,37 @@ namespace iTasks.Models
 {
     public class Task
     {
+        public int Id { get; set; }
+        public int IdManager { get; set; }
+        public int IdProgrammer { get; set; }
+        public string OrderExecution { get; set; }
+        public char Description { get; set; }
+        public DateTime DateStart { get; set; }
+        public  DateTime DateEnd { get; set; }
+        public int IdTypeTask { get; set; }
+        public int StoryPoints { get; set; }
+        public DateTime RealTimeStart { get; set; }
+        public DateTime RealTimeEnd { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string CurrentState  { get; set; }
+        public Task()
+        {
+
+        }
+        public Task(int idManager, int idProgrammer, string orderExecution, char description, DateTime dateStart, DateTime dateEnd, int idTypeTask, int storyPoints, DateTime realTimeStart, DateTime realTimeEnd, DateTime creationTime, string currentState)
+        {
+            IdManager = idManager;
+            IdProgrammer = idProgrammer;
+            OrderExecution = orderExecution;
+            Description = description;
+            DateStart = dateStart;
+            DateEnd = dateEnd;
+            IdTypeTask = idTypeTask;
+            StoryPoints = storyPoints;
+            RealTimeStart = realTimeStart;
+            RealTimeEnd = realTimeEnd;
+            CreationTime = creationTime;
+            CurrentState = currentState;
+        }
     }
 }
