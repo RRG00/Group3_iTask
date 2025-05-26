abe7cc12772a1ec490b1b75c30f7ef3c994f9dbe
Guilherme Sá <2241539@my.ipleiria.pt>
2025-05-26 20:59:39 +0100

    TypeTask
    TypeTask Done

--- Views/frmGereTiposTarefas.cs
+++ Views/frmGereTiposTarefas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using iTasks.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,39 @@ namespace iTasks
 {
     public partial class frmGereTiposTarefas : Form
     {
+        private ITaskContext ItaskContext;
         public frmGereTiposTarefas()
         {
             InitializeComponent();
+            ItaskContext = new ITaskContext();       
+        }
+
+        private void btGravar_Click(object sender, EventArgs e)
+        {
+            using (var ItaskContext = new ITaskContext())
+            {
+                string Name = txtDesc.Text;
+                TypeTask typeTask = new TypeTask(Name);
+                ItaskContext.TipeTasks.Add(typeTask);
+                ItaskContext.SaveChanges();
+
+                UpdateFields();
+            }
+        }
+        public void UpdateFields()
+        {
+            using (var ItaskContext = new ITaskContext())
+            {
+                lstLista.DataSource = ItaskContext.TipeTasks.ToList();
+            }
+        }
+        public void UpdateTypeTaskList()
+        {
+            using (var ItaskContext = new ITaskContext())
+            {
+                lstLista.DataSource = null;
+                lstLista.DataSource = ItaskContext.Manager.ToList();
+            }
         }
     }
 }
