9684c10a0af03f20e58b93cf59e7096ceca954b8
Guilherme Sá <2241539@my.ipleiria.pt>
2025-06-03 19:25:12 +0100

    Updates no FormKanBan
    Button de mover Tarefa para ToDo, e de Reniciar Tarefa

--- Views/frmKanban.cs
+++ Views/frmKanban.cs
@@ -13,24 +13,28 @@ namespace iTasks
 {
     public partial class frmKanban : Form
     {
-       
+        private List<iTasks.Models.Task> listaToDo;
+        private List<iTasks.Models.Task> listaDoing;
+
         public frmKanban()
         {
+          
             InitializeComponent();
 
             using (ITaskContext context = new ITaskContext())
             {
-                if (context.Tasks.Count() == 0)
+                if (!context.Tasks.Any())
                 {
-                    context.Tasks.Add(new Models.Task(1, 1, "1", "Exemplo de Task", DateTime.Now,DateTime.Now,1,21,DateTime.Now,DateTime.Now,DateTime.Now,"ToDo"));
+                    context.Tasks.Add(new Models.Task(1, 1, "1", "Exemplo de Task", DateTime.Now, DateTime.Now, 1, 21, DateTime.Now, DateTime.Now, DateTime.Now, "ToDo"));
                     context.SaveChanges();
                 }
-                UpdateTypeTaskList();
             }
 
+            UpdateStateTaskList();
         }
+        
 
-       
+
         public void UpdateTypeTaskList()
         {
             using (var ItaskContext = new ITaskContext())
@@ -39,10 +43,26 @@ namespace iTasks
                 lstTodo.DataSource = ItaskContext.Tasks.ToList();
             }
         }
+        public void UpdateStateTaskList()
+        {
+            using (var ItaskContext = new ITaskContext())
+            {
+                listaToDo = ItaskContext.Tasks.Where(t => t.CurrentState == "ToDo").ToList();
+                listaDoing = ItaskContext.Tasks.Where(t => t.CurrentState == "Doing").ToList();
+
+                lstTodo.DataSource = null;
+                lstTodo.DataSource = listaToDo;
+
+                lstDoing.DataSource = null;
+                lstDoing.DataSource = listaDoing;
+            }
+        }
+
+
 
         private void frmKanban_Load(object sender, EventArgs e)
         {
-           
+
         }
 
 
@@ -90,7 +110,7 @@ namespace iTasks
         private void lstTodo_SelectedIndexChanged(object sender, EventArgs e)
         {
             int index = lstTodo.SelectedIndex;
-            if (index == -1) return ;
+            if (index == -1) return;
 
             iTasks.Models.Task selectedTask = (iTasks.Models.Task)lstTodo.Items[index];
 
@@ -101,6 +121,63 @@ namespace iTasks
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btSetDoing_Click(object sender, EventArgs e)
+        {
+            if (lstTodo.SelectedItem != null)
+            {
+                var task = (iTasks.Models.Task)lstTodo.SelectedItem;
+
+                using (var context = new ITaskContext())
+                {
+                    // Busca a tarefa pelo ID no contexto atual
+                    var taskDb = context.Tasks.Find(task.Id);
+                    if (taskDb != null)
+                    {
+                        taskDb.CurrentState = "Doing";
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tarefa não encontrada no banco de dados.");
+                    }
+                }
+
+                UpdateStateTaskList();
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma tarefa para executar.");
+            }
+        }
+
+        private void btSetTodo_Click(object sender, EventArgs e)
+        {
+            if (lstDoing.SelectedItem != null)
+            {
+                var task = (iTasks.Models.Task)lstDoing.SelectedItem;
+
+                using (var context = new ITaskContext())
+                {
+                    var taskDb = context.Tasks.Find(task.Id);
+                    if (taskDb != null)
+                    {
+                        taskDb.CurrentState = "ToDo";
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tarefa não encontrada no banco de dados.");
+                    }
+                }
+
+                UpdateStateTaskList();
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma tarefa em 'Doing' para reniciar a Tarefa");
+            }
         }
     }
 }
